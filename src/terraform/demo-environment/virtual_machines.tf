50f5ec6c90644f21eec0a340da7d415bfabaaee8
Leigh <leigh.shayler@gmail.com>
2022-07-15 00:44:21 +0800

    Update virtual_machines.tf

--- src/terraform/demo-environment/virtual_machines.tf
+++ src/terraform/demo-environment/virtual_machines.tf
@@ -30,6 +30,8 @@ resource "azurerm_linux_virtual_machine" "example" {
   location            = var.resource_location
   size                = "Standard_F2"
   admin_username      = "adminuser"
+  admin_password      = var.vm_password
+  disable_password_authentication = false
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
